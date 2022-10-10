2fb8daa8946af819d75044408ea48ffb4825a837
Vanessa Christy Chandra <vanessa.chandra2002@gmail.com>
2022-10-10 22:12:46 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 
 import Homepage from "./components/pages/homepage/Homepage";
 import EditTrip from "./components/pages/edittrip/EditTrip";
@@ -16,37 +16,129 @@ import SignIn from "./components/pages/signin/SignIn";
 import SignUp from "./components/pages/signup/SignUp";
 import Preference from "./components/pages/preference/Preference";
 import Preference2 from "./components/pages/preference2/Preference2";
-import MapComponent from "./components/pages/gMap/map"
+import MapComponent from "./components/pages/gMap/map";
 import NavigationBar from "./components/pages/header/header";
 import Sidebar from "./components/pages/sidebar/Sidebar";
 
 function App() {
-
-  const [menuOpen, setMenuOpen] = useState(0)
+  const [menuOpen, setMenuOpen] = useState(0);
 
   return (
     <div className="App">
-     
-    
-      <BrowserRouter>
-      <NavigationBar meneuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Sidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <NavigationBar meneuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Sidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Routes>
-          <Route path="/homepage" element={<Homepage/>}></Route>
-          <Route path="/edittrip" element={<EditTrip/>}></Route>
-          <Route path="/choosedestination" element={<div><ChooseDestination/></div>}></Route>
-          <Route path="/destination" element={<div><Destination/></div>}></Route>
-          <Route path="/carousel" element={<div><CarouselPage/></div>}></Route>
-          <Route path="/edittrip" element={<div><EditTrip/></div>}></Route>
-          <Route path="/addrecommendation" element={<div><AddRecommendation/></div>}></Route>
-          <Route path="/edittripreccs" element={<div><EditTripReccs/></div>}></Route>
-          <Route path="/tourpackage" element={<div><TourPackage/></div>}></Route>
-          <Route path="/editprofile" element={<div><EditProfile/></div>}></Route>
-          <Route path="/mydocuments" element={<div><MyDocuments/></div>}></Route>
-          <Route path="/" element={<div><SignIn/></div>}></Route>
-          <Route path="/signup" element={<div><SignUp/></div>}></Route>
-          <Route path="/preference" element={<div><Preference/></div>}></Route>
-          <Route path="/preference2" element={<div><Preference2/></div>}></Route>
+          <Route
+            basename={process.env.PUBLIC_URL}
+            path="/homepage"
+            element={<Homepage />}
+          ></Route>
+          <Route path="/edittrip" element={<EditTrip />}></Route>
+          <Route
+            path="/choosedestination"
+            element={
+              <div>
+                <ChooseDestination />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/destination"
+            element={
+              <div>
+                <Destination />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/carousel"
+            element={
+              <div>
+                <CarouselPage />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/edittrip"
+            element={
+              <div>
+                <EditTrip />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/addrecommendation"
+            element={
+              <div>
+                <AddRecommendation />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/edittripreccs"
+            element={
+              <div>
+                <EditTripReccs />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/tourpackage"
+            element={
+              <div>
+                <TourPackage />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/editprofile"
+            element={
+              <div>
+                <EditProfile />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/mydocuments"
+            element={
+              <div>
+                <MyDocuments />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={
+              <div>
+                <SignIn />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/signup"
+            element={
+              <div>
+                <SignUp />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/preference"
+            element={
+              <div>
+                <Preference />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/preference2"
+            element={
+              <div>
+                <Preference2 />
+              </div>
+            }
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
